3d4846f78ee2805815ef8a033b62a3ac33519c97
edvaldo <costaedy18@gmail.com>
2024-10-18 23:14:46 +0100

    update

---
+++ The_FrontEnd_Hardskills/GeekForGeekTutorialReact/geekForGeeksReact/src/Element.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+let i = 1;
+
+const Element = () => {
+  return (
+    <h1>{i == 1 ? "Hello World!" : "False!"} </h1>
+  )
+}
+
+export default Element

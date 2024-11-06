483d7f758148ae3b5cb05e16b05aa7d75f1e1ed0
Edvaldo vunge <costaedy18@gmail.com>
2024-11-06 21:00:23 +0000

    install npm

--- The_FrontEnd_Hardskills/GeekForGeekTutorialReact/geekForGeeksReact/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Element from './Element.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Element />
-  </StrictMode>,
-)

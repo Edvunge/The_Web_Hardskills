ead1cb4a94eb3b17a5ce707a631efee3a3a31ecf
edvaldovunge <costaedy18@gmail.com>
2025-07-02 14:11:38 +0100

    Paginas Not Found , Entendendo SEO

--- FullStack_Package/sect01_DominandoNextJs_completoDoZero/my-app/src/app/layout.tsx
+++ FullStack_Package/sect01_DominandoNextJs_completoDoZero/my-app/src/app/layout.tsx
@@ -1,5 +1,25 @@
 import "./globals.css";
 import { Header } from "@/components/header";
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: 'Aula Next JS do zero!',
+  description: 'Aprendendo Next JS do zero!',
+  openGraph: {
+    title: 'Aprendendo Next JS no youtube!',
+    description: 'Aprendendo Next JS do zero!',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    }
+  }
+}
+
 
 export default function RootLayout({
   children,
